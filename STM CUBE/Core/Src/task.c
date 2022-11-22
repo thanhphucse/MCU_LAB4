0210bda691d5fe9c2e65b9206c897df61242ef87
ThanhPhu999 <phu.nguyen03022002@hcmut.edu.vn>
2022-11-22 23:58:32 +0700

    update

--- STM CUBE/Core/Src/task.c
+++ STM CUBE/Core/Src/task.c
@@ -12,19 +12,19 @@ void blink_led1(){//tasks running periodically in 0.5 second
 }
 
 void blink_led2(){//tasks running periodically in 1 second
-	HAL_GPIO_TogglePin(LED1_GPIO_Port,LED2_Pin);
+	HAL_GPIO_TogglePin(LED2_GPIO_Port,LED2_Pin);
 }
 
 void blink_led3(){//tasks running periodically in 1.5 second
-	HAL_GPIO_TogglePin(LED1_GPIO_Port,LED3_Pin);
+	HAL_GPIO_TogglePin(LED3_GPIO_Port,LED3_Pin);
 }
 
 void blink_led4(){//tasks running periodically in 2 second
-	HAL_GPIO_TogglePin(LED1_GPIO_Port,LED4_Pin);
+	HAL_GPIO_TogglePin(LED4_GPIO_Port,LED4_Pin);
 }
 
 //one-shot task
 void blink_led5(){
-	HAL_GPIO_TogglePin(LED1_GPIO_Port,LED5_Pin);
+	HAL_GPIO_TogglePin(LED5_GPIO_Port,LED5_Pin);
 }
 
